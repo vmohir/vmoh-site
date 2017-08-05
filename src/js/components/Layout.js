94532f6d735e95e948d8e49fc0bd999c0d323d67
Vahid Mohammadi <email@vmoh.ir>
2017-08-06 01:48:34 +0430

    debug showSection and add handleHoverMiddleImage

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -16,27 +16,44 @@ export default class Layout extends React.Component {
 		}
 	}
 
-	showSection(section) {
+	showSection = (section) => {
 		const { fullname, bio } = this.state;
-		switch (section) {
-			case "Resume":
-			return (<Resume />);
-			
-			case "Profile":
-			return (<Profile fullname={fullname} bio={bio} />);
+		if (typeof section == "string")
+			section = [section];
+		let result = null;
+		if (section) {
+			result = [];
+			section.forEach((section,index)=>{
+				switch (section) {
+					case "Resume":
+					result.push((<Resume />));
+					break;
+					
+					case "Profile":
+					result.push((<Profile fullname={fullname} bio={bio} />));
+					break;
 
-			case "Profile":
-			return (<Portfolio />);
+					case "Portfolio":
+					result.push((<Portfolio />));
+					break;
+				}
+			});
 		}
+		return result;
 	}
 
 	render() {
 		const { showSection, fullname, bio } = this.state;
 		return (
-			<div>
-				<Middle_image fullname={fullname} bio={bio} />
-				{ showSection ? this.showSection(showSection) : null}
+			<div id="react-root">
+				<Middle_image fullname={fullname} bio={bio} handleMouseOver={this.handleHoverMiddleImage} />
+				{this.showSection(showSection)}
 			</div>
 		);
+	}
+
+	handleHoverMiddleImage = () => {
+		this.setState({showSection: ["Resume","Profile","Portfolio"]});
+		console.log(this.state.showSection);
 	}
 }
