b0864c55094a996c39c5298f321e9b164e5764ee
Vahid Mohammadi <email@vmoh.ir>
2017-08-08 02:41:07 +0430

    no changes

--- src/js/client.js
+++ src/js/client.js
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import $ from 'jquery';
 
 require('../css/main.scss');
-
-// window.jQuery = window.$ = $;
 
 import Layout from "./components/Layout"
 
