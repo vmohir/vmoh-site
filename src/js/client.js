23eb9790d4093da6a11ab7b67df493ccd57e52f7
Vahid Mohammadi <email@vmoh.ir>
2017-08-15 00:05:53 +0430

    no changes :|

--- src/js/client.js
+++ src/js/client.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 
 require('../css/main.scss');
 
-import Layout from "./components/Layout"
+import Layout from "./components/Layout";
+
+require('./general-scripts.js');
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
