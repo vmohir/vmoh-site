0f002a706d1a74aae33dc9674b6be8cb11725d5e
Vahid Mohammadi <email@vmoh.ir>
2017-08-15 00:06:57 +0430

    a lot of changes

--- src/js/pages/Portfolio.js
+++ src/js/pages/Portfolio.js
@@ -8,18 +8,34 @@ export default class Portfolio extends React.Component {
 		super();
 		this.state = {
 			gallery: [
-				{img: "image-url", title: "title", desc: "description about image"}
+				{img: "/images/img1.jpg", title: "title", desc: "description about image"},
+				{img: "/images/img2.jpg", title: "the new title", desc: "the new description"},
+				{img: "/images/img2.jpg", title: "the new title", desc: "the new description"},
+				{img: "/images/img2.jpg", title: "the new title", desc: "the new description"},
 			]
 		}
 	}
 
 	render() {
 		const { gallery } = this.state;
-		const galleryComp = gallery.map((media, index)=>{
-			return <PortfolioMedia {...media} key={index} />
-		})
+		const { id:section_id } = this.props;
+		let galleryComp = gallery.map((media, index) => <PortfolioMedia {...media} key={index} />);
 		return (
-			<div>{galleryComp}</div>
+			<div id={section_id}>
+				<button className="vertical-slider-nav-btn vertical-slider-nav-top"><span className="glyphicon glyphicon-chevron-up"></span></button>
+				<div className="vertical-slider-container">
+					<ul>{galleryComp}</ul>
+				</div>
+				<button className="vertical-slider-nav-btn vertical-slider-nav-bottom"><span className="glyphicon glyphicon-chevron-down"></span></button>
+			</div>
 		);
+	}
+
+	componentDidMount() {
+		this.initiateVerticalSlider();
+	}
+
+	initiateVerticalSlider = () => {
+		$('.vertical-slider-item:first-child').addClass('vertical-slider-item-active');
 	}
 }
