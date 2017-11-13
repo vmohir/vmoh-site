a794c1784010566377d9d51b033fc9eb5d1ff5c2
Vahid Mohammadi <email@vmoh.ir>
2017-11-13 15:30:29 +0330

    No changes

--- src/js/functions.js
+++
@@ -1,8 +0,0 @@
-export default function delay(delay) {
-	var ctr, rej, p = new Promise(function (resolve, reject) {
-        ctr = setTimeout(resolve, delay);
-        rej = reject;
-    });
-    p.cancel = function(){ clearTimeout(ctr); rej(Error("Cancelled"))};
-    return p;
-}
