e40122a818a9891c45dcc5a5ef7002eb27f2d0cc
Vahid <14050191+vmohir@users.noreply.github.com>
2026-01-04 05:45:41 +0000

    feat: add astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import preact from '@astrojs/preact';
 
+import compress from 'astro-compress';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()]
+  integrations: [preact(), compress()]
 });
